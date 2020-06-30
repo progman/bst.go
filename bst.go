a8831a1239fcd728020218fb61b787c24b311f9f
progman <gnuplanet@gmail.com>
2020-06-30 20:45:33 +0300

    update

---
+++ bst.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+ "fmt"
+// "math"
+)
+/*
+type Vertex struct {
+ X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+ return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+*/
+
+type bst_item_t struct {
+
+	key int
+}
+
+func (p bst_item_t) init(key int) {
+
+	p.key = key
+}
+
+
+
+type bst_t struct {
+	head *bst_item_t
+}
+
+func (p bst_t) init() {
+
+	p.head = nil
+}
+
+
+
+func (p bst_t) insert(key int) {
+
+	var bstr_item bst_item_t
+	bstr_item.init(key)
+
+	if (p.head == nil) {
+
+		p.head = &bstr_item
+		return
+	}
+/*
+	for {
+
+		if p.head.key < val
+	}
+*/
+}
+
+
+func main() {
+
+	var bst bst_t
+	bst.init()
+
+
+	bst.insert(1)
+	bst.insert(2)
+	bst.insert(3)
+
+
+//	v := Vertex{3, 4}
+//	fmt.Println(v.Abs())
+	fmt.Println("ok")
+}
