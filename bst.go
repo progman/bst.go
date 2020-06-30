914a97d4128a50cb7bf6caff81e2acbc3dbeeb5b
progman <gnuplanet@gmail.com>
2020-06-30 23:41:03 +0300

    update

--- bst.go
+++ bst.go
@@ -4,15 +4,6 @@ import (
  "fmt"
 // "math"
 )
-/*
-type Vertex struct {
- X, Y float64
-}
-
-func (v Vertex) Abs() float64 {
- return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-*/
 
 type bst_item_t struct {
 
@@ -21,7 +12,7 @@ type bst_item_t struct {
 	r *bst_item_t
 }
 
-func (p bst_item_t) init(key int) {
+func (p *bst_item_t) init(key int) {
 
 	p.key = key
 	p.l   = nil
@@ -34,26 +25,46 @@ type bst_t struct {
 	head *bst_item_t
 }
 
-func (p bst_t) init() {
+func (p *bst_t) init() {
 
 	p.head = nil
 }
 
 
 
-func (p bst_t) insert(key int) {
+func (p *bst_t) insert(key int) {
 
 	fmt.Printf("insert(%d)\n", key)
-	var bstr_item bst_item_t
-	bstr_item.init(key)
+	var p_bstr_item *bst_item_t = &bst_item_t{}
+	p_bstr_item.init(key)
+
+
+//	if (p.head == nil) {
+//		fmt.Println("== nil")
+//	} else {
+//		fmt.Println("!= nil")
+//	}
 
 
 	if (p.head == nil) {
 
 		fmt.Println("set as root")
-		p.head = &bstr_item
+		p.head = p_bstr_item
+
+//		fmt.Printf("key %d\n", p_bstr_item.key)
+//		fmt.Printf("key %d\n", p.head.key)
+
+
+//	if (p.head == nil) {
+//		fmt.Println("== nil")
+//	} else {
+//		fmt.Println("!= nil")
+//	}
+
 		return
 	}
+
+
 
 
 	var p_cur *bst_item_t = p.head
@@ -63,9 +74,13 @@ func (p bst_t) insert(key int) {
 		if (key > p_cur.key) {
 
 			if (p_cur.r == nil) {
-				p_cur.r = &bstr_item
+
+				fmt.Println("set rigth")
+				p_cur.r = p_bstr_item
 				break;
 			} else {
+
+				fmt.Println("go to rigth")
 				p_cur = p_cur.r
 			}
 		}
@@ -74,9 +89,13 @@ func (p bst_t) insert(key int) {
 		if (key < p_cur.key) {
 
 			if (p_cur.l == nil) {
-				p_cur.l = &bstr_item
+
+				fmt.Println("set left")
+				p_cur.l = p_bstr_item
 				break;
 			} else {
+
+				fmt.Println("go to left")
 				p_cur = p_cur.l
 			}
 		}
@@ -84,10 +103,11 @@ func (p bst_t) insert(key int) {
 // equal way (we can skip it or change), change it
 		if (key == p_cur.key) {
 
+			fmt.Println("swap")
 			var p_old *bst_item_t = p_cur
-			p_cur = &bstr_item
-			bstr_item.l = p_old
-			bstr_item.r = p_old.r
+			p_cur = p_bstr_item
+			p_bstr_item.l = p_old
+			p_bstr_item.r = p_old.r
 			p_old.r = nil
 			break
 		}
@@ -101,9 +121,19 @@ func main() {
 	bst.init()
 
 
-	bst.insert(1)
-	bst.insert(2)
-	bst.insert(3)
+//	bst.insert(20)
+//	bst.insert(20)
+
+	bst.insert(100)
+
+	bst.insert(110)
+	bst.insert(90)
+
+	bst.insert(120)
+	bst.insert(80)
+
+	bst.insert(130)
+	bst.insert(70)
 
 
 //	v := Vertex{3, 4}
